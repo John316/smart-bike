b724683beb0b693c9e95e9caad59b43c3424ad23
Ivan Oleynik <ivano@marka-software.com>
2016-05-20 10:32:34 +0300

    Change display. Use new OLCD. Add voice module

--- smart-bike.ino
+++ smart-bike.ino
@@ -1,141 +1,331 @@
 // Defined LCD Display
-#include <LiquidCrystal.h>
-LiquidCrystal  lcd (8, 9, 4, 5, 6, 7);
+#include <Adafruit_ssd1306syp.h>
 
-// Defined Servo motor
-#include "Servo.h"
-Servo servoMain;
 
-  int reed = A11;
+#include <Wtv020sd16p.h>
+#include <Servo.h>
 
-// Defined Magnit
-int magnitPin = 22;
+//define OLCD
+#define SDA_PIN A8
+#define SCL_PIN A9
+Adafruit_ssd1306syp display(SDA_PIN, SCL_PIN);
 
-// Defined Giro
+// define Servo motor 
+Servo backGearServo;
 
-// Defined temp vareble
+// define voice module
+int resetPin = 7;  // The pin number of the reset pin 7.
+int clockPin = 6;  // The pin number of the clock pin 8.
+int dataPin = 5;  // The pin number of the data pin 5.
+int busyPin = 4;  // The pin number of the busy pin 4.
+Wtv020sd16p wtv020sd16p(resetPin,clockPin,dataPin,busyPin);
+
+
+
+// temperatura
+#include "DHT.h"
+#define DHTPIN 48     // к какому пину будет подключен вывод Data
+
+//выбор используемого датчика
+#define DHTTYPE DHT11   // DHT 11 
+//#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+//инициализация датчика
+DHT dht(DHTPIN, DHTTYPE);
+
+
+// define Magnit
+// int magnitPin = 42;
+// Interapt
+int interrupt = 0;
+volatile int state = 0;
+
+// define Giro
+
+// define temp vareble
 long timer = 0;
-long prevTime = 0;
-long prevTime2 = 0;
+int magnitTimeout = 0;
+int localState = 0;
+
+// VoiceComment
+long prevTime3 = 0;
+int IsSay = 0;
+
+// Clock 
 long prevmicros = 0;//переменная для хранения значений таймера
 int sek = 0; //значение секунд
 int minu = 0; //значение минут
 int chas = 0; //значение часов
 boolean counter = false; // счетчик для полусекунд
+boolean noSayHello = true;
 
-int magnitDetect;
-
-// Defined main vareble
+// define main vareble
 int currentGear = 1;
 double oborot = 2.1;
 double KM = 0.00;
-double maxSpeed;
+int maxSpeed;
 double distance = 0.00;
+bool stopBike = false;
+
+void magnitBlink()
+{
+  state++;
+}
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Start");
-
-  pinMode(magnitPin, INPUT);
   
-  // setup LCD Display
-  lcd.begin(16, 2);
-  lcd.clear();
-  lcd.write("Smart Bike");
-  lcd.setCursor(0, 2);
-  lcd.write("Welcome!");
-  delay(1000);
+  dht.begin();
+  wtv020sd16p.reset();
 
+  attachInterrupt(interrupt, magnitBlink, FALLING);
+  
   // setup Servo motor
-  //servoMain.attach(24);
-
+  backGearServo.attach(2);
+  
+  // OLED 
+  display.initialize();
+  display.clear();
+  display.setTextColor(WHITE);
+  display.setTextSize(2);
+  display.println("Smart Bike");
+  display.println("Welcome!");
+  display.update();
+  
+  if(noSayHello == true)
+    SeyHello();
+    
+  delay(3000);
+ 
+  DisplayInfo();
+  
   // setup Giro
 
-  // setup Interrupt
-  //attachInterrupt(magnitPin, CalcSpeed, RISING);
 }
 
 void loop() {
+	// Calc Time and Speed
+	CalcTime();
+}
 
-  //int sign = digitalRead(magnitPin);
-  //Serial.println("sign");
-  //Serial.println(sign);
-  //delay(500);
+void SeyHello()
+{
+  wtv020sd16p.asyncPlayVoice(0);
+  noSayHello = false;
+}
 
-  int magnitDetect = digitalRead(magnitPin);
-  long timer2 = millis();
-  long diff = timer2 - prevTime2;
-  if(magnitDetect == 0 && diff > 100){
-    CalcSpeed();
-    prevTime2 = timer2;
+void CalcTime() {
+
+  if(!stopBike){
+    if (micros() - prevmicros > 500000)
+    { 
+      prevmicros = micros();  //принимает значение каждые полсекунды
+      counter = !counter;
+      if (counter == false)
+      { 
+        sek++;    //переменная секунда + 1
+		
+		CalcSpeed();
+		DisplayInfo();
+      }
+      else
+      {
+	   
+      }
+  
+      if (sek > 59) //если переменная секунда больше 59 ...
+      {
+        sek = 0; //сбрасываем ее на 0
+        minu++;//пишем +1 в переменную минута
+      }
+      if (minu > 59) //если переменная минута больше 59 ...
+      {
+        minu = 0; //сбрасываем ее на 0
+        chas++;//пишем +1 в переменную час
+      }
+      if (chas > 23) //если переменная час больше 23 ...
+      {
+        chas = 0; //сбрасываем ее на 0
+      }
+      //PrintTime();
+    }
+  }else if(state > 0){
+	stopBike = false;
+	magnitTimeout = 0;
   }
   
-  SpeedController();
-
-  // Read button
-  ReadButton();
-
-  // Read sensor
-  ReadSensor();
-
-  // Calc Time
-  CalcTime();
+  //if 3 sec we have no signal from magnit
+  if(magnitTimeout == 3){
+    stopBike = true;
+    KM = 0;
+	magnitTimeout++;
+	DisplayInfo();
+  }
 }
 
 // Main Logic
 
-void DisplayInfo() {
-  lcd.clear();
-  lcd.print("Gear:");
-  lcd.setCursor(5, 0);
-  lcd.print(currentGear);
-
-  // Display current speed
-  lcd.setCursor(9, 0);
-  lcd.print("KM/H:");
-  lcd.setCursor(14, 0);
+void CalcSpeed() {
+  localState = state;
+  state = 0;
+  
+  if(localState > 0){
+	double ms =  state * oborot / (magnitTimeout + 1);
+	KM = ms * 3.6; // meter / sec and conver to km/h
+	magnitTimeout = 0;
+	stopBike = false;
+  }else{
+	magnitTimeout++
+  }
+  
+  // Calc Distance
+  CalcDistance();
   int KMH = ceil(KM);
-  lcd.print(KMH);
+  CalcMaxSpeed(KMH);
+  SpeedController(KMH);
+  VoiceComment(KMH);
 }
 
-void ReadButton() {
+void CalcDistance() {
+  distance += oborot / 1000;
 }
 
-void ReadSensor() {
-  // read Magnit
-  //int magnitSignal = digitalRead(magnitPin);
 
-  // call Speed calc
+void DisplayInfo() {
+  display.clear();
+  display.setCursor(0, 0);  
+  PrintTime();
+  PrintGear();
+  PrintSpeed();
+  PrintTermo();  
+  PrintDistance();
+  PrintMaxSpeed();
 
+  display.update();
+}
+
+void PrintGear()
+{
+  display.setTextSize(2);
+  display_print("  G");
+  display_println(currentGear);
+}
+
+void PrintTermo()
+{
+    // температура
+    int h = dht.readHumidity();
+    int t = dht.readTemperature();
+  
+    // проверяем правильные ли данные получили
+    if (isnan(t) || isnan(h)) {
+      Serial.println("Error reading from DHT");
+    } else {
+      display.setCursor(66, 22);
+      display.setTextSize(1);  
+      display_print(t);
+      display_print(" *C ");
+      display_print(h);
+      display_print(" %");
+    }
+}
+
+void PrintSpeed()
+{
+  // Display current speed
+  int KMH = ceil(KM);
+  display.setTextSize(5);
+  display.setCursor(0, 22);
+  display_println(KMH);
+}
+
+void PrintMaxSpeed()
+{
+  display.setCursor(66, 53);
+  display.setTextSize(1);
+  display_print("Max:");
+  display_println(maxSpeed);
+}
+
+void PrintDistance()
+{
+  display.setCursor(66, 37);
+  display.setTextSize(1);
+  display_print("Dist: ");
+  display_println(distance);
+}
+
+void PrintTime()
+{
+    display.setTextSize(2);
+    //if (chas>=0 && chas<10) {
+     //display_print("0");
+     //display_print(chas);
+    //}else display_print(chas);
+
+     //display_print(":");
+    display_print(" ");
+    if (minu >= 0 && minu < 10) {
+      display_print("0");
+      display_print(minu);
+    }
+    else display_print(minu);
+    
+    display_print(":");
+    
+    if (sek >= 0 && sek < 10) {
+      display_print("0");
+      display_print(sek);
+    }
+    else display_print(sek);
+}
+  
   // read giro
   // set angule correction;
   // active Signalization
 }
 
-void SpeedController() {
-  if (KM > 1 && KM < 4) {
-    if (currentGear != 5)
-      SpeedSteps(5);
-  } else if (KM >= 5 && KM < 10) {
-    if (currentGear != 4)
-      SpeedSteps(4);
-  } else if (KM >= 10 && KM < 15) {
+void SpeedController(int KMH) {
+  if (KMH > 1 && KMH < 5) {
+    if (currentGear != 1){
+      //GearSteps(1);
+    }
+  } else if (KMH >= 5 && KMH < 7) {
+    if (currentGear != 2){
+      //GearSteps(2);
+    }
+  } else if (KMH >= 7 && KMH < 10) {
     if (currentGear != 3)
-      SpeedSteps(3);
-  } else if (KM >= 15 && KM < 20) {
-    if (currentGear != 2)
-      SpeedSteps(2);
-  } else if (KM >= 20 && KM < 25) {
-    if (currentGear != 1)
-      SpeedSteps(1);
+      GearSteps(3);
+  } else if (KM >= 10 && KMH < 15) {
+    if (currentGear != 4)
+      GearSteps(4);
+  } else if (KM >= 15 && KMH < 20) {
+    if (currentGear != 5)
+      GearSteps(5);
+  }else if (KM >= 20 && KMH < 25) {
+    if (currentGear != 6)
+      GearSteps(6);
+  }else if (KM >= 25 && KMH < 30) {
+    if (currentGear != 7)
+      GearSteps(7);
+  }else if (KM > 30) {
+    if (currentGear != 8)
+      GearSteps(8);
   }
 }
 
-void SpeedSteps(int num) {
-  if (num + 1 == currentGear || num - 1 == currentGear)
-    makeStep(num);
-  else
-    needCorrection(num);
+void GearSteps(int num) {
+
+  makeStep(num);
+
+  // TODO: Correction don't work.
+  //if (num == currentGear + 1 || num == currentGear - 1)
+    //makeStep(num);
+  //else
+    //needCorrection(num);
 }
 
 void needCorrection(int num)
@@ -148,104 +338,176 @@ void needCorrection(int num)
 
 void makeStep(int num) {
   if (num == 1)
-    servoMain.write(0);
+    TurnFirstGear(currentGear);
   else if (num == 2)
-    servoMain.write(45);
+    TurnSecondGear(currentGear);
   else if (num == 3)
-    servoMain.write(90);
+    TurnThirdGear(currentGear);
   else if (num == 4)
-    servoMain.write(135);
+    TurnFourthGear(currentGear);
   else if (num == 5)
-    servoMain.write(180);
+    TurnFifthGear(currentGear);
+  else if (num == 6)
+    TurnSixthGear(currentGear);
+  else if (num == 7)
+    TurnSeventhGear(currentGear);
+  else if (num == 8)
+    TurnEighthGear(currentGear);
 
   currentGear = num;
 }
 
-void CalcSpeed() {
-  timer = millis();
-  double diff = timer - prevTime; // difference in second between two signal from magnit detector
-  double a = diff / 1000;
-  double ms =  oborot / a;
-  KM = ms * 3.6; // meter / sec and conver to km/h
-  prevTime = timer;
+void TurnFirstGear(int currentGear){
+  if (currentGear > 1)
+  {
+    /* code */
+  }else{
 
-  // Calc Distance
-  CalcDistance();
-  CalcMaxSpeed();
+  }
+  wtv020sd16p.asyncPlayVoice(1);
 }
 
-void CalcMaxSpeed() {
-  if (maxSpeed < currentGear) {
-    maxSpeed = currentGear;
+void TurnSecondGear(int currentGear){
+  if (currentGear > 2)
+  {
+    /* code */
+  }else{
+
+  }
+  wtv020sd16p.asyncPlayVoice(2);
+}
+
+void TurnThirdGear(int currentGear){
+  if (currentGear > 3)
+  {
+    /* code */
+  }else{
+
+  }
+  wtv020sd16p.asyncPlayVoice(3);
+}
+
+void TurnFourthGear(int currentGear){
+  if (currentGear > 4)
+  {
+    /* code */
+  }else{
+
+  }
+  wtv020sd16p.asyncPlayVoice(4);
+}
+
+void TurnFifthGear(int currentGear){
+  if (currentGear > 5)
+  {
+    /* code */
+  }else{
+
+  }
+  wtv020sd16p.asyncPlayVoice(5);
+}
+
+void TurnSixthGear(int currentGear){
+  if (currentGear > 6)
+  {
+    /* code */
+  }else{
+
+  }
+  wtv020sd16p.asyncPlayVoice(6);
+}
+
+void TurnSeventhGear(int currentGear){
+  if (currentGear > 7)
+  {
+    /* code */
+  }else{
+
+  }
+  wtv020sd16p.asyncPlayVoice(7);
+}
+
+void TurnEighthGear(int currentGear){
+  if (currentGear > 8)
+  {
+    /* code */
+  }else{
+
+  }
+  wtv020sd16p.asyncPlayVoice(8);
+}
+
+void CalcMaxSpeed(int KMH) {
+  if (maxSpeed < KMH) {
+    maxSpeed = KMH;
   }
 }
 
-void CalcTime() {
-  if (micros() - prevmicros > 500000)
-  { 
-    prevmicros = micros();  //принимает значение каждые полсекунды
-    counter = !counter;
-    if (counter == false)
-    { sek++;              //переменная секунда + 1
-      lcd.setCursor(2, 1);
-      lcd.print(":");         //выводим символ ":"между часами и минутами
-      lcd.setCursor(5, 1);
-      lcd.print(":");         //выводим символ ":"между  минутами и секундами
-    }
-    else
-    {
-      DisplayInfo();
-      lcd.setCursor(2, 1);
-      lcd.print(" ");    // мигание :
-      lcd.setCursor(5, 1);
-      lcd.print(" ");    // мигание :
-    }
-
-    if (sek > 59) //если переменная секунда больше 59 ...
-    {
-      sek = 0; //сбрасываем ее на 0
-      minu++;//пишем +1 в переменную минута
-    }
-    if (minu > 59) //если переменная минута больше 59 ...
-    {
-      minu = 0; //сбрасываем ее на 0
-      chas++;//пишем +1 в переменную час
-    }
-    if (chas > 23) //если переменная час больше 23 ...
-    {
-      chas = 0; //сбрасываем ее на 0
-    }
-
-    lcd.setCursor(0, 1); //выводим значение часов
-    if (chas >= 0 && chas < 10) {
-      lcd.print("0");
-      lcd.print(chas);
-    }//количество часов
-    else lcd.print(chas);
-
-    lcd.setCursor(3, 1); //выводим значение минут
-    if (minu >= 0 && minu < 10) {
-      lcd.print("0");
-      lcd.print(minu);
-    }//количество минут
-    else lcd.print(minu);
-
-    lcd.setCursor(6, 1); //выводим значение секунд
-    if (sek >= 0 && sek < 10) {
-      lcd.print("0");
-      lcd.print(sek);
-    }//количество секунд
-    else lcd.print(sek);
+void VoiceComment(int KMH)
+{
+  long diff = timer - prevTime3;
+  if(diff > 10000){
+    IsSay = 0;
   }
-}
+    if(KMH > 17 && KMH < 19 && IsSay == 0){
+      prevTime3 = timer;
+      wtv020sd16p.asyncPlayVoice(10);
+      IsSay = 1;
+    }
+    if(KMH > 20 && KMH < 22 && IsSay == 0){
+      prevTime3 = timer;
+      wtv020sd16p.asyncPlayVoice(12);
+      IsSay = 1;
+    }
+    if(KMH > 31 && KMH < 32 && IsSay == 0){
+      wtv020sd16p.asyncPlayVoice(30);
+      IsSay = 1;
+      prevTime3 = timer;
+    }
+    if(KMH > 41 && KMH < 42){
+      wtv020sd16p.asyncPlayVoice(40);
+      IsSay = 1;
+      prevTime3 = timer;
+    }
+    if(KMH > 50){
+      wtv020sd16p.asyncPlayVoice(50);
+      wtv020sd16p.asyncPlayVoice(11);
+      IsSay = 1;
+      prevTime3 = timer;
+    }
+  }
 
-void CalcDistance() {
-  distance += oborot / 1000;
-}
 
 // Additional Logic
 
 void ActiveSignalization() {
 }
 
+void display_println(String mess) {
+  display.println(mess);
+}
+
+void display_println(int num) {
+  if(num >= 0)
+    display.println(num);
+}
+
+void display_print(int num) {
+  if(num >= 0)
+    display.print(num);
+}
+
+void display_print(double num) {
+  if(num >= 0)
+    display.print(num);
+}
+
+void display_println(double num) {
+  if(num >= 0)
+    display.println(num);
+}
+
+void display_print(String mess) {
+  display.print(mess);
+}
 
